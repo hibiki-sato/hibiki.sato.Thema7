05a39882174fd73e0f151fa56419263d9c3c27a8
akio0911 <akio0911@gmail.com>
2021-09-18 15:43:21 +0900

    feedback

--- Thema7/Thema7/RedViewController.swift
+++ Thema7/Thema7/RedViewController.swift
@@ -12,17 +12,16 @@ class RedViewController: UIViewController {
     @IBOutlet private weak var textField1: UITextField!
     @IBOutlet private weak var textField2: UITextField!
     @IBOutlet private weak var resultLabel: UILabel!
-    
+
     @IBAction private func didTapButton(_ sender: Any) {
         // 入力
         let num1 = Int(textField1.text ?? "") ?? 0
         let num2 = Int(textField2.text ?? "") ?? 0
-        // 計算
-        let result = String(num1 + num2)
-        // 表示
-        resultLabel.text = result
+
+        resultLabel.text = String(num1 + num2)
+
         // キーボードを隠す
-        self.textField1.resignFirstResponder()
-        self.textField2.resignFirstResponder()
+        textField1.resignFirstResponder()
+        textField2.resignFirstResponder()
     }
 }
